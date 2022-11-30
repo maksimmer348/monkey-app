e8b6c4848d5c25b5afb24e01985be78753adb38a
maksimmer348 <maksimmer348@gmail.com>
2022-11-30 16:15:31 +0300

    отображение данных

---
+++ MonkeyApp/View/DetailsPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace MonkeyApp.View;
+
+public partial class DetailsPage : ContentPage
+{
+	public DetailsPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MonkeyApp/View/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MonkeyApp;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
+
