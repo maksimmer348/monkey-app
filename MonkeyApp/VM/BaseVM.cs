03a1156ef535a9177e0b246d94931e6146a3a2d2
maksimmer348 <maksimmer348@gmail.com>
2022-12-01 10:52:06 +0300

    стандартынй mvvm

--- MonkeyApp/VM/BaseVM.cs
+++ MonkeyApp/VM/BaseVM.cs
@@ -1,12 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MonkeyApp.VM
 {
-    class BaseVM
+    class BaseVM : INotifyPropertyChanged
     {
+        bool isBusy;
+        public bool IsBusy
+        {
+            get => isBusy;
+            set
+            {
+                if (isBusy == value) return;
+                isBusy = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(IsNotBusy));
+            }
+        }
+
+        string title;
+
+        public string Title
+        {
+            get => title;
+            set
+            {
+                if (title == value) return;
+                title = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public bool IsNotBusy => !IsBusy;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName]string name = null)
+        {
+           PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+
     }
 }
--- MonkeyApp/VM/MonkeyDetailsVM.cs
+++ MonkeyApp/VM/MonkeyDetailsVM.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace MonkeyApp.VM
 {
-    class MonkeyDetailsVM
+    class MonkeyDetailsVM : BaseVM
     {
+
     }
 }
